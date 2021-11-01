e2d3fa8113ddc0b6979a6da181103a825d4b2d60
phongthepham <phamthephong83@gmail.com>
2021-11-01 23:05:16 +0700

    Complete Register Test

--- src/test/java/RegisterTest.java
+++ src/test/java/RegisterTest.java
@@ -34,13 +34,12 @@ public class RegisterTest {
     }
 
     @BeforeMethod
-    public void BeforeMethod()
-    {
+    public void BeforeMethod() {
         webDriver.get("https://alada.vn/tai-khoan/dang-ky.html");
     }
 
     @Test
-    public void TC_01_RegisterWithEmptyDataTest() {
+    public void TC_01_RegisterWithEmptyData() {
         WebElement btnDangKy = webDriver.findElement(byBtnDangKy);
         btnDangKy.click();
 
@@ -64,7 +63,7 @@ public class RegisterTest {
     }
 
     @Test
-    public void TC_02_RegisterWithInvalidEmailTest(){
+    public void TC_02_RegisterWithInvalidEmail() {
         WebElement txtHoVaTen = webDriver.findElement(byTxtHoVaTen);
         txtHoVaTen.sendKeys("Phong Pham");
 
@@ -91,6 +90,127 @@ public class RegisterTest {
 
         WebElement lblNhapLaiEmailError = webDriver.findElement(byLblNhapLaiEmailError);
         Assert.assertEquals(lblNhapLaiEmailError.getText(), "Email nhập lại không đúng");
+    }
+
+    @Test
+    public void TC_03_RegisterWithIncorrectConfirmEmail() {
+        WebElement txtHoVaTen = webDriver.findElement(byTxtHoVaTen);
+        txtHoVaTen.sendKeys("Phong Pham");
+
+        WebElement txtEmail = webDriver.findElement(byTxtEmail);
+        txtEmail.sendKeys("abc@gmail.com");
+
+        WebElement txtNhapLaiEmail = webDriver.findElement(byTxtNhapLaiEmail);
+        txtNhapLaiEmail.sendKeys("abc@yahoo.com");
+
+        WebElement txtMatKhau = webDriver.findElement(byTxtMatKhau);
+        txtMatKhau.sendKeys("12345678");
+
+        WebElement txtNhapLaiMatKhau = webDriver.findElement(byTxtNhapLaiMatKhau);
+        txtNhapLaiMatKhau.sendKeys("12345678");
+
+        WebElement txtDienThoai = webDriver.findElement(byTxtDienThoai);
+        txtDienThoai.sendKeys("0983111111");
+
+        WebElement btnDangKy = webDriver.findElement(byBtnDangKy);
+        btnDangKy.click();
+
+        WebElement lblNhapLaiEmailError = webDriver.findElement(byLblNhapLaiEmailError);
+        Assert.assertEquals(lblNhapLaiEmailError.getText(), "Email nhập lại không đúng");
+    }
+
+    @Test
+    public void TC_04_RegisterWithPasswordLessThan6Characters() {
+        WebElement txtHoVaTen = webDriver.findElement(byTxtHoVaTen);
+        txtHoVaTen.sendKeys("Phong Pham");
+
+        WebElement txtEmail = webDriver.findElement(byTxtEmail);
+        txtEmail.sendKeys("abc@gmail.com");
+
+        WebElement txtNhapLaiEmail = webDriver.findElement(byTxtNhapLaiEmail);
+        txtNhapLaiEmail.sendKeys("abc@gmail.com");
+
+        WebElement txtMatKhau = webDriver.findElement(byTxtMatKhau);
+        txtMatKhau.sendKeys("12345");
+
+        WebElement txtNhapLaiMatKhau = webDriver.findElement(byTxtNhapLaiMatKhau);
+        txtNhapLaiMatKhau.sendKeys("12345");
+
+        WebElement txtDienThoai = webDriver.findElement(byTxtDienThoai);
+        txtDienThoai.sendKeys("0983111111");
+
+        WebElement btnDangKy = webDriver.findElement(byBtnDangKy);
+        btnDangKy.click();
+
+        WebElement lblMatKhauError = webDriver.findElement(byLblMatKhauError);
+        Assert.assertEquals(lblMatKhauError.getText(), "Mật khẩu phải có ít nhất 6 ký tự");
+
+        WebElement lblNhapLaiMatKhauError = webDriver.findElement(byLblNhapLaiMatKhauError);
+        Assert.assertEquals(lblNhapLaiMatKhauError.getText(), "Mật khẩu phải có ít nhất 6 ký tự");
+    }
+
+    @Test
+    public void TC_05_RegisterWithIncorrectConfirmPassword(){
+        WebElement txtHoVaTen = webDriver.findElement(byTxtHoVaTen);
+        txtHoVaTen.sendKeys("Phong Pham");
+
+        WebElement txtEmail = webDriver.findElement(byTxtEmail);
+        txtEmail.sendKeys("abc@gmail.com");
+
+        WebElement txtNhapLaiEmail = webDriver.findElement(byTxtNhapLaiEmail);
+        txtNhapLaiEmail.sendKeys("abc@gmail.com");
+
+        WebElement txtMatKhau = webDriver.findElement(byTxtMatKhau);
+        txtMatKhau.sendKeys("1234567");
+
+        WebElement txtNhapLaiMatKhau = webDriver.findElement(byTxtNhapLaiMatKhau);
+        txtNhapLaiMatKhau.sendKeys("12345678");
+
+        WebElement txtDienThoai = webDriver.findElement(byTxtDienThoai);
+        txtDienThoai.sendKeys("0983111111");
+
+        WebElement btnDangKy = webDriver.findElement(byBtnDangKy);
+        btnDangKy.click();
+
+        WebElement lblNhapLaiMatKhauError = webDriver.findElement(byLblNhapLaiMatKhauError);
+        Assert.assertEquals(lblNhapLaiMatKhauError.getText(), "Mật khẩu bạn nhập không khớp");
+    }
+
+    @Test
+    public void TC_06_RegisterWithInvalidPhoneNumber(){
+        WebElement txtHoVaTen = webDriver.findElement(byTxtHoVaTen);
+        txtHoVaTen.sendKeys("Phong Pham");
+
+        WebElement txtEmail = webDriver.findElement(byTxtEmail);
+        txtEmail.sendKeys("abc@gmail.com");
+
+        WebElement txtNhapLaiEmail = webDriver.findElement(byTxtNhapLaiEmail);
+        txtNhapLaiEmail.sendKeys("abc@gmail.com");
+
+        WebElement txtMatKhau = webDriver.findElement(byTxtMatKhau);
+        txtMatKhau.sendKeys("12345678");
+
+        WebElement txtNhapLaiMatKhau = webDriver.findElement(byTxtNhapLaiMatKhau);
+        txtNhapLaiMatKhau.sendKeys("12345678");
+
+        WebElement txtDienThoai = webDriver.findElement(byTxtDienThoai);
+        txtDienThoai.sendKeys("098123456");
+
+        WebElement btnDangKy = webDriver.findElement(byBtnDangKy);
+        btnDangKy.click();
+
+        WebElement lblDienThoaiError = webDriver.findElement(byLblDienThoaiError);
+        Assert.assertEquals(lblDienThoaiError.getText(), "Số điện thoại phải từ 10-11 số.");
+
+        txtDienThoai = webDriver.findElement(byTxtDienThoai);
+        txtDienThoai.clear();
+        txtDienThoai.sendKeys("1234567890");
+
+        btnDangKy = webDriver.findElement(byBtnDangKy);
+        btnDangKy.click();
+
+        lblDienThoaiError = webDriver.findElement(byLblDienThoaiError);
+        Assert.assertEquals(lblDienThoaiError.getText(), "Số điện thoại bắt đầu bằng: 09 - 03 - 012 - 016 - 018 - 019");
     }
 
     @AfterClass

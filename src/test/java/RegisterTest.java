fd5d2c66903ed5e0c94ce48cbe5bbc3ec2620571
phongthepham <phamthephong83@gmail.com>
2021-11-09 02:37:59 +0700

    Complete Register Test

--- src/test/java/RegisterTest.java
+++ src/test/java/RegisterTest.java
@@ -6,10 +6,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.time.Duration;
+
 //import static org.testng.Assert.assertEquals;
 
 public class RegisterTest {
-    WebDriver webDriver;
+    WebDriver driver;
 
     By byBtnDangKy = By.cssSelector("button[type='submit']");
 
@@ -29,192 +31,193 @@ public class RegisterTest {
 
     @BeforeClass
     public void BeforeClass() {
-        WebDriverManager.chromedriver().setup();
-        webDriver = new ChromeDriver();
+        WebDriverManager.chromedriver().setup();    // use WebDriverManager from github
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));  // use this in Selenium 4.0
+        driver = new ChromeDriver();
     }
 
     @BeforeMethod
     public void BeforeMethod() {
-        webDriver.get("https://alada.vn/tai-khoan/dang-ky.html");
+        driver.get("https://alada.vn/tai-khoan/dang-ky.html");
     }
 
     @Test
     public void TC_01_RegisterWithEmptyData() {
-        WebElement btnDangKy = webDriver.findElement(byBtnDangKy);
+        WebElement btnDangKy = driver.findElement(byBtnDangKy);
         btnDangKy.click();
 
-        WebElement lblHoVaTenError = webDriver.findElement(byLblHoVaTenError);
+        WebElement lblHoVaTenError = driver.findElement(byLblHoVaTenError);
         Assert.assertEquals(lblHoVaTenError.getText(), "Vui lòng nhập họ tên");
 
-        WebElement lblEmailError = webDriver.findElement(byLblEmailError);
+        WebElement lblEmailError = driver.findElement(byLblEmailError);
         Assert.assertEquals(lblEmailError.getText(), "Vui lòng nhập email");
 
-        WebElement lblNhapLaiEmailError = webDriver.findElement(byLblNhapLaiEmailError);
+        WebElement lblNhapLaiEmailError = driver.findElement(byLblNhapLaiEmailError);
         Assert.assertEquals(lblNhapLaiEmailError.getText(), "Vui lòng nhập lại địa chỉ email");
 
-        WebElement lblMatKhauError = webDriver.findElement(byLblMatKhauError);
+        WebElement lblMatKhauError = driver.findElement(byLblMatKhauError);
         Assert.assertEquals(lblMatKhauError.getText(), "Vui lòng nhập mật khẩu");
 
-        WebElement lblNhapLaiMatKhauError = webDriver.findElement(byLblNhapLaiMatKhauError);
+        WebElement lblNhapLaiMatKhauError = driver.findElement(byLblNhapLaiMatKhauError);
         Assert.assertEquals(lblNhapLaiMatKhauError.getText(), "Vui lòng nhập lại mật khẩu");
 
-        WebElement lblDienThoaiError = webDriver.findElement(byLblDienThoaiError);
+        WebElement lblDienThoaiError = driver.findElement(byLblDienThoaiError);
         Assert.assertEquals(lblDienThoaiError.getText(), "Vui lòng nhập số điện thoại.");
     }
 
     @Test
     public void TC_02_RegisterWithInvalidEmail() {
-        WebElement txtHoVaTen = webDriver.findElement(byTxtHoVaTen);
+        WebElement txtHoVaTen = driver.findElement(byTxtHoVaTen);
         txtHoVaTen.sendKeys("Phong Pham");
 
-        WebElement txtEmail = webDriver.findElement(byTxtEmail);
+        WebElement txtEmail = driver.findElement(byTxtEmail);
         txtEmail.sendKeys("123@456@789");
 
-        WebElement txtNhapLaiEmail = webDriver.findElement(byTxtNhapLaiEmail);
+        WebElement txtNhapLaiEmail = driver.findElement(byTxtNhapLaiEmail);
         txtNhapLaiEmail.sendKeys("123@456@789");
 
-        WebElement txtMatKhau = webDriver.findElement(byTxtMatKhau);
+        WebElement txtMatKhau = driver.findElement(byTxtMatKhau);
         txtMatKhau.sendKeys("12345678");
 
-        WebElement txtNhapLaiMatKhau = webDriver.findElement(byTxtNhapLaiMatKhau);
+        WebElement txtNhapLaiMatKhau = driver.findElement(byTxtNhapLaiMatKhau);
         txtNhapLaiMatKhau.sendKeys("12345678");
 
-        WebElement txtDienThoai = webDriver.findElement(byTxtDienThoai);
+        WebElement txtDienThoai = driver.findElement(byTxtDienThoai);
         txtDienThoai.sendKeys("0983111111");
 
-        WebElement btnDangKy = webDriver.findElement(byBtnDangKy);
+        WebElement btnDangKy = driver.findElement(byBtnDangKy);
         btnDangKy.click();
 
-        WebElement lblEmailError = webDriver.findElement(byLblEmailError);
+        WebElement lblEmailError = driver.findElement(byLblEmailError);
         Assert.assertEquals(lblEmailError.getText(), "Vui lòng nhập email hợp lệ");
 
-        WebElement lblNhapLaiEmailError = webDriver.findElement(byLblNhapLaiEmailError);
+        WebElement lblNhapLaiEmailError = driver.findElement(byLblNhapLaiEmailError);
         Assert.assertEquals(lblNhapLaiEmailError.getText(), "Email nhập lại không đúng");
     }
 
     @Test
     public void TC_03_RegisterWithIncorrectConfirmEmail() {
-        WebElement txtHoVaTen = webDriver.findElement(byTxtHoVaTen);
+        WebElement txtHoVaTen = driver.findElement(byTxtHoVaTen);
         txtHoVaTen.sendKeys("Phong Pham");
 
-        WebElement txtEmail = webDriver.findElement(byTxtEmail);
+        WebElement txtEmail = driver.findElement(byTxtEmail);
         txtEmail.sendKeys("abc@gmail.com");
 
-        WebElement txtNhapLaiEmail = webDriver.findElement(byTxtNhapLaiEmail);
+        WebElement txtNhapLaiEmail = driver.findElement(byTxtNhapLaiEmail);
         txtNhapLaiEmail.sendKeys("abc@yahoo.com");
 
-        WebElement txtMatKhau = webDriver.findElement(byTxtMatKhau);
+        WebElement txtMatKhau = driver.findElement(byTxtMatKhau);
         txtMatKhau.sendKeys("12345678");
 
-        WebElement txtNhapLaiMatKhau = webDriver.findElement(byTxtNhapLaiMatKhau);
+        WebElement txtNhapLaiMatKhau = driver.findElement(byTxtNhapLaiMatKhau);
         txtNhapLaiMatKhau.sendKeys("12345678");
 
-        WebElement txtDienThoai = webDriver.findElement(byTxtDienThoai);
+        WebElement txtDienThoai = driver.findElement(byTxtDienThoai);
         txtDienThoai.sendKeys("0983111111");
 
-        WebElement btnDangKy = webDriver.findElement(byBtnDangKy);
+        WebElement btnDangKy = driver.findElement(byBtnDangKy);
         btnDangKy.click();
 
-        WebElement lblNhapLaiEmailError = webDriver.findElement(byLblNhapLaiEmailError);
+        WebElement lblNhapLaiEmailError = driver.findElement(byLblNhapLaiEmailError);
         Assert.assertEquals(lblNhapLaiEmailError.getText(), "Email nhập lại không đúng");
     }
 
     @Test
     public void TC_04_RegisterWithPasswordLessThan6Characters() {
-        WebElement txtHoVaTen = webDriver.findElement(byTxtHoVaTen);
+        WebElement txtHoVaTen = driver.findElement(byTxtHoVaTen);
         txtHoVaTen.sendKeys("Phong Pham");
 
-        WebElement txtEmail = webDriver.findElement(byTxtEmail);
+        WebElement txtEmail = driver.findElement(byTxtEmail);
         txtEmail.sendKeys("abc@gmail.com");
 
-        WebElement txtNhapLaiEmail = webDriver.findElement(byTxtNhapLaiEmail);
+        WebElement txtNhapLaiEmail = driver.findElement(byTxtNhapLaiEmail);
         txtNhapLaiEmail.sendKeys("abc@gmail.com");
 
-        WebElement txtMatKhau = webDriver.findElement(byTxtMatKhau);
+        WebElement txtMatKhau = driver.findElement(byTxtMatKhau);
         txtMatKhau.sendKeys("12345");
 
-        WebElement txtNhapLaiMatKhau = webDriver.findElement(byTxtNhapLaiMatKhau);
+        WebElement txtNhapLaiMatKhau = driver.findElement(byTxtNhapLaiMatKhau);
         txtNhapLaiMatKhau.sendKeys("12345");
 
-        WebElement txtDienThoai = webDriver.findElement(byTxtDienThoai);
+        WebElement txtDienThoai = driver.findElement(byTxtDienThoai);
         txtDienThoai.sendKeys("0983111111");
 
-        WebElement btnDangKy = webDriver.findElement(byBtnDangKy);
+        WebElement btnDangKy = driver.findElement(byBtnDangKy);
         btnDangKy.click();
 
-        WebElement lblMatKhauError = webDriver.findElement(byLblMatKhauError);
+        WebElement lblMatKhauError = driver.findElement(byLblMatKhauError);
         Assert.assertEquals(lblMatKhauError.getText(), "Mật khẩu phải có ít nhất 6 ký tự");
 
-        WebElement lblNhapLaiMatKhauError = webDriver.findElement(byLblNhapLaiMatKhauError);
+        WebElement lblNhapLaiMatKhauError = driver.findElement(byLblNhapLaiMatKhauError);
         Assert.assertEquals(lblNhapLaiMatKhauError.getText(), "Mật khẩu phải có ít nhất 6 ký tự");
     }
 
     @Test
     public void TC_05_RegisterWithIncorrectConfirmPassword(){
-        WebElement txtHoVaTen = webDriver.findElement(byTxtHoVaTen);
+        WebElement txtHoVaTen = driver.findElement(byTxtHoVaTen);
         txtHoVaTen.sendKeys("Phong Pham");
 
-        WebElement txtEmail = webDriver.findElement(byTxtEmail);
+        WebElement txtEmail = driver.findElement(byTxtEmail);
         txtEmail.sendKeys("abc@gmail.com");
 
-        WebElement txtNhapLaiEmail = webDriver.findElement(byTxtNhapLaiEmail);
+        WebElement txtNhapLaiEmail = driver.findElement(byTxtNhapLaiEmail);
         txtNhapLaiEmail.sendKeys("abc@gmail.com");
 
-        WebElement txtMatKhau = webDriver.findElement(byTxtMatKhau);
+        WebElement txtMatKhau = driver.findElement(byTxtMatKhau);
         txtMatKhau.sendKeys("1234567");
 
-        WebElement txtNhapLaiMatKhau = webDriver.findElement(byTxtNhapLaiMatKhau);
+        WebElement txtNhapLaiMatKhau = driver.findElement(byTxtNhapLaiMatKhau);
         txtNhapLaiMatKhau.sendKeys("12345678");
 
-        WebElement txtDienThoai = webDriver.findElement(byTxtDienThoai);
+        WebElement txtDienThoai = driver.findElement(byTxtDienThoai);
         txtDienThoai.sendKeys("0983111111");
 
-        WebElement btnDangKy = webDriver.findElement(byBtnDangKy);
+        WebElement btnDangKy = driver.findElement(byBtnDangKy);
         btnDangKy.click();
 
-        WebElement lblNhapLaiMatKhauError = webDriver.findElement(byLblNhapLaiMatKhauError);
+        WebElement lblNhapLaiMatKhauError = driver.findElement(byLblNhapLaiMatKhauError);
         Assert.assertEquals(lblNhapLaiMatKhauError.getText(), "Mật khẩu bạn nhập không khớp");
     }
 
     @Test
     public void TC_06_RegisterWithInvalidPhoneNumber(){
-        WebElement txtHoVaTen = webDriver.findElement(byTxtHoVaTen);
+        WebElement txtHoVaTen = driver.findElement(byTxtHoVaTen);
         txtHoVaTen.sendKeys("Phong Pham");
 
-        WebElement txtEmail = webDriver.findElement(byTxtEmail);
+        WebElement txtEmail = driver.findElement(byTxtEmail);
         txtEmail.sendKeys("abc@gmail.com");
 
-        WebElement txtNhapLaiEmail = webDriver.findElement(byTxtNhapLaiEmail);
+        WebElement txtNhapLaiEmail = driver.findElement(byTxtNhapLaiEmail);
         txtNhapLaiEmail.sendKeys("abc@gmail.com");
 
-        WebElement txtMatKhau = webDriver.findElement(byTxtMatKhau);
+        WebElement txtMatKhau = driver.findElement(byTxtMatKhau);
         txtMatKhau.sendKeys("12345678");
 
-        WebElement txtNhapLaiMatKhau = webDriver.findElement(byTxtNhapLaiMatKhau);
+        WebElement txtNhapLaiMatKhau = driver.findElement(byTxtNhapLaiMatKhau);
         txtNhapLaiMatKhau.sendKeys("12345678");
 
-        WebElement txtDienThoai = webDriver.findElement(byTxtDienThoai);
+        WebElement txtDienThoai = driver.findElement(byTxtDienThoai);
         txtDienThoai.sendKeys("098123456");
 
-        WebElement btnDangKy = webDriver.findElement(byBtnDangKy);
+        WebElement btnDangKy = driver.findElement(byBtnDangKy);
         btnDangKy.click();
 
-        WebElement lblDienThoaiError = webDriver.findElement(byLblDienThoaiError);
+        WebElement lblDienThoaiError = driver.findElement(byLblDienThoaiError);
         Assert.assertEquals(lblDienThoaiError.getText(), "Số điện thoại phải từ 10-11 số.");
 
-        txtDienThoai = webDriver.findElement(byTxtDienThoai);
+        txtDienThoai = driver.findElement(byTxtDienThoai);
         txtDienThoai.clear();
         txtDienThoai.sendKeys("1234567890");
 
-        btnDangKy = webDriver.findElement(byBtnDangKy);
+        btnDangKy = driver.findElement(byBtnDangKy);
         btnDangKy.click();
 
-        lblDienThoaiError = webDriver.findElement(byLblDienThoaiError);
+        lblDienThoaiError = driver.findElement(byLblDienThoaiError);
         Assert.assertEquals(lblDienThoaiError.getText(), "Số điện thoại bắt đầu bằng: 09 - 03 - 012 - 016 - 018 - 019");
     }
 
     @AfterClass
     public void AfterClass() {
-        webDriver.quit();
+        driver.quit();
     }
 }
